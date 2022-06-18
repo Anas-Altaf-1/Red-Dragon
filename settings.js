2a07b4fb21617a731e05f10c069d4e81e00e034f
Anas-Altaf-1 <105292709+Anas-Altaf-1@users.noreply.github.com>
2022-06-18 09:13:14 +0500

    Zit settings

--- settings.js
+++ settings.js
@@ -20,30 +20,30 @@ global.APIKeys = {
 
 //customize settings\\
 
-global.alivemsg = `My name is RED DRAGON🔥
-My Creator is Kumuthu Prabhasha😎` //Costomize Alive Message (type your message in `` )
+global.alivemsg = `My name is KING ZIT🔥
+My Creator is ANAS ALTAF😎` //Costomize Alive Message (type your message in `` )
 
-global.owner = ['+380944814219'] //Owner number in aive msg
-global.premium = ['+380944814219'] //Owner Number info
-global.ownernomer = '+380944814219' //Owner Number <<<
+global.owner = ['+923336681780'] //Owner number in aive msg
+global.premium = ['+923336681780'] //Owner Number info
+global.ownernomer = '+923336681780' //Owner Number <<<
 
-global.ownername = '😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖' //Owner Name
-global.botname = ' ☬RED༒DRAGON' //Bot Name
+global.ownername = '😎ANAS ALTAF' //Owner Name
+global.botname = ' KING༒ZIT' //Bot Name
 
-global.button = '🤭මේක ටච් කරන්න📍' //Costomize A Button Name In Alive Message
-global.btnurl = '🙂🔥දැන් සැපද?😑' //Change Your Button's URL
+global.button = 'TALK TO ME📍' //Costomize A Button Name In Alive Message
+global.btnurl = 'wa.me/923336681780' //Change Your Button's URL
 
 global.region = 'India, South India, Kerala' //Country Region
 
-global.packname = '☬RED༒DRAGON࿐' //Bot Pacage Name
-global.author = '😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖❯❯' //Author Name
+global.packname = 'KING༒ZIT࿐' //Bot Pacage Name
+global.author = '😎ANAS ALTAF' //Author Name
 global.prefa = ['','!','.','😂','❤','💠'] //Command Prefix eg:(.alive ,alive 😂alive)
 global.sessionName = 'session' //Session Name!
-global.sp = '⭔' //A Charactor In Menu!
+global.sp = '∆' //A Charactor In Menu!
 
 
 global.ig = 'https://github.com/prabhasha2006' //Github Account!
-global.footer = '©_Kumuthu.' // DO NOT CHANGE!
+global.footer = '©KINGZIT.' // DO NOT CHANGE!
 
 global.mess = { // Error Mesages
     success: 'Done ✅',
@@ -60,16 +60,16 @@ global.mess = { // Error Mesages
 // DO NOT CHANGE \\
     global.limitawal = {
     premium: "Infinity",
-    free: 12,
+    free: 1000,
     monayawal: 1000
 }
    global.rpg = {
-   darahawal: 100,
-   besiawal: 15,
-   goldawal: 10,
-   emeraldawal: 5,
-   umpanawal: 5,
-   potionawal: 1
+   darahawal: 1000,
+   besiawal: 1000,
+   goldawal: 1000,
+   emeraldawal: 1000,
+   umpanawal: 1000,
+   potionawal: 1000
 }
 global.thumb = fs.readFileSync('./DragonMedia/image/red-_-dragon-kumuthu.jpg')
 global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
